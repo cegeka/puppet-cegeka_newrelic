5a2d63d3df92c6caaf1c783a324229299a1df32b
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-06-12 10:03:16 +0200

    Added provisioning of application agent monitoring

--- manifests/application_monitoring.pp
+++ manifests/application_monitoring.pp
@@ -4,6 +4,35 @@
 #
 # Parameters:
 #
+# [*application_root_dir*] The New Relic agent installation directory (no default).
+# - Required: yes
+# - Content: String
+#
+# [*application_owner*] The owner of the New Relic agent installation directory (default: 'root').
+# - Required: yes
+# - Content: String
+#
+# [*application_group*] The group of the New Relic agent installation directory (default: 'root').
+# - Required: yes
+# - Content: String
+#
+# [*newrelic_license_key*] 40-character hexadecimal string provided by New Relic. This is required in order for the server monitor to start (no default).
+# - Required: yes
+# - Content: String
+#
+# [*newrelic_app_name*] Set the name of your application as you'd like it show up in New Relic (default: 'My Application').
+# - Required: yes
+# - Content: String
+#
+# [*newrelic_agent_loglevel*] The agent uses its own log file to keep its logging separate from that of your application (default: 'info').
+# - Required: no
+# - Content: 'off' | 'severe' | 'warning' | 'info' | 'fine' | 'finer' | 'finest'
+#
+# [*newrelic_use_ssl*] The agent communicates with New Relic via http by default.  If you want to communicate via https to increase security, 
+#                      then turn on SSL by setting this value to true (default: false).
+# - Required: no
+# - Content: true | false
+#
 # Sample Usage:
 #
 # class { 'newrelic::application_monitoring':
@@ -21,38 +50,43 @@ class newrelic::application_monitoring($application_root_dir=undef, $application
     fail("The license key associated with your New Relic account must be provided")
   }
 
+  if ! ($newrelic_agent_loglevel in ['off' , 'severe' , 'warning' , 'info' , 'fine' , 'finer' , 'finest']) {
+    fail("${newrelic_agent_loglevel} is not one of valid predefined values for agent loglevels")
+  }
+
+  case $newrelic_use_ssl {
+    true, false: { $newrelic_use_ssl_real = $newrelic_use_ssl }
+    default: {
+      fail("Newrelic::Application_monitoring[${newrelic_use_ssl}]: parameter newrelic_use_ssl must be a boolean")
+    }
+  }
+
   file { "${application_root_dir}/newrelic" :
     ensure => directory,
-    owner  => "${application_owner}",
-    group  => "${application_group}",
+    owner  => $application_owner,
+    group  => $application_group,
   }
 
   file { "${application_root_dir}/newrelic/logs" :
     ensure  => directory,
-    owner   => "${application_owner}",
-    group   => "${application_group}",
+    owner   => $application_owner,
+    group   => $application_group,
     require => File["${application_root_dir}/newrelic"],
   }
 
   file { "${application_root_dir}/newrelic/newrelic.jar" :
     ensure  => file,
-    owner   => "${application_owner}",
-    group   => "${application_group}",
+    owner   => $application_owner,
+    group   => $application_group,
     source  => "puppet:///modules/${module_name}/newrelic.jar",
     require => File["${application_root_dir}/newrelic"],
   }
 
   file { "${application_root_dir}/newrelic/newrelic.yml" :
     ensure  => file,
-    owner   => "${application_owner}",
-    group   => "${application_group}",
+    owner   => $application_owner,
+    group   => $application_group,
     content => template("${module_name}/application/newrelic.yml.erb"),
   }
-
-
-# /opt/newrelic/ voor logs ? mss beter onder /opt/appserver/newrelic/logs ?
-# /opt/appserver/newrelic/newrelic.jar
-# /opt/appserver/newrelic/newrelic.yml
-
 
 }
---
+++ spec/classes/newrelic_application_monitoring_spec.rb
@@ -0,0 +1,99 @@
+#!/usr/bin/env rspec
+
+require 'spec_helper'
+
+describe 'newrelic::application_monitoring' do
+
+  context 'without an installation root directory specified' do
+
+		it {
+      expect { subject }.to raise_error(
+        Puppet::Error, /The root directory of the application server installation must be provided/
+    )}
+
+	end
+
+  context 'without a license_key specified' do
+
+		let (:params) { { :application_root_dir => '/opt/appserver' } }
+
+    it {
+    	expect { subject }.to raise_error(
+      	Puppet::Error, /The license key associated with your New Relic account must be provided/
+  	)}
+
+  end
+
+  context 'without a valid agent loglevel' do
+
+		let (:params) { { :application_root_dir => '/opt/appserver', 
+											:newrelic_license_key => '1234567890' , 
+											:newrelic_agent_loglevel => 'vverbosedebug'} }
+
+		it {
+    	expect { subject }.to raise_error(
+      	Puppet::Error, /vverbosedebug is not one of valid predefined values for agent loglevels/
+  	)}
+
+	end
+
+  context 'without a valid use_ssl value' do
+
+		let (:params) { { :application_root_dir => '/opt/appserver', 
+                      :newrelic_license_key => '1234567890' , 
+                      :newrelic_agent_loglevel => 'fine',
+											:newrelic_use_ssl => 'bla'} }
+
+		it {
+      expect { subject }.to raise_error(
+        Puppet::Error, /parameter newrelic_use_ssl must be a boolean/
+    )}
+
+	end
+
+	context 'with a valid installation directory and license_key' do
+	
+		let (:params) { { :application_root_dir => '/opt/appserver',
+											:newrelic_license_key => '1234567890' } }
+
+		it { should contain_class 'newrelic::application_monitoring' }
+		it { should contain_file '/opt/appserver/newrelic' }
+		it { should contain_file '/opt/appserver/newrelic/newrelic.jar' }
+		it { should contain_file '/opt/appserver/newrelic/newrelic.yml' }
+
+	end
+
+	context 'with a valid installation directory, application owner and group, license_key and agent loglevel' do
+
+		let (:params) { { :application_root_dir => '/opt/appserver',
+											:application_owner => 'newrelic',
+											:application_group => 'newrelic',
+											:newrelic_license_key => '1234567890', 
+											:newrelic_agent_loglevel => 'finer' } }
+
+    it { should contain_class 'newrelic::application_monitoring' }
+		it do 
+    	should contain_file('/opt/appserver/newrelic').with({
+      	'ensure' => 'directory',
+      	'owner'  => 'newrelic',
+      	'group'  => 'newrelic',
+    	})
+  	end
+		it do
+      should contain_file('/opt/appserver/newrelic/newrelic.jar').with({
+        'ensure' => 'file',
+        'owner'  => 'newrelic',
+        'group'  => 'newrelic',
+      })
+    end
+		it do
+      should contain_file('/opt/appserver/newrelic/newrelic.yml').with({
+        'ensure' => 'file',
+        'owner'  => 'newrelic',
+        'group'  => 'newrelic',
+      })
+    end
+
+	end
+
+end
